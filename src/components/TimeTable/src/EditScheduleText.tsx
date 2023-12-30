08a29ca50183b30c09b6e1127dc50c9057ec37fb
KingHyunSu <kailking611@gmail.com>
2023-12-31 02:00:37 +0900

    시간표 일정 제목 제스처 버그 수정

--- src/components/TimeTable/src/EditScheduleText.tsx
+++ src/components/TimeTable/src/EditScheduleText.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, TextInput, NativeSyntheticEvent, TextInputContentSizeChangeEventData} from 'react-native'
+import {StyleSheet, TextInput} from 'react-native'
 
 import {Gesture, GestureDetector} from 'react-native-gesture-handler'
 import Animated, {useSharedValue, useAnimatedStyle, runOnJS} from 'react-native-reanimated'
@@ -15,8 +15,7 @@ interface Props {
   onChangeSchedule: Function
 }
 const EditScheduleText = ({data, centerX, centerY, radius, titleInputRef, onChangeSchedule}: Props) => {
-  const containerPadding = 0
-  const wrapperPadding = 5
+  const containerPadding = 50
   const borderWidth = 1
 
   const [top, setTop] = React.useState(0)
@@ -31,13 +30,11 @@ const EditScheduleText = ({data, centerX, centerY, radius, titleInputRef, onChan
   }
 
   const containerX = useSharedValue(
-    Math.round(centerX - (containerPadding + wrapperPadding + borderWidth) + (radius / 100) * data.title_x)
+    Math.round(centerX - (containerPadding + borderWidth) + (radius / 100) * data.title_x)
   )
   const containerY = useSharedValue(
-    Math.round(centerY - (containerPadding + wrapperPadding + borderWidth) - (radius / 100) * data.title_y)
+    Math.round(centerY - (containerPadding + borderWidth) - (radius / 100) * data.title_y)
   )
-  const containerWidth = useSharedValue(0)
-  const containerHeight = useSharedValue(0)
   const containerRotate = useSharedValue(data.title_rotate)
   const conatinerSavedRotate = useSharedValue(0)
 
@@ -51,9 +48,8 @@ const EditScheduleText = ({data, centerX, centerY, radius, titleInputRef, onChan
       const changedX = containerX.value
       const changedY = containerY.value
 
-      const padding = containerPadding + wrapperPadding
-      const xPercentage = Math.round(((changedX + padding - centerX) / radius) * 100)
-      const yPercentage = Math.round((((changedY + padding - centerY) * -1) / radius) * 100)
+      const xPercentage = Math.round(((changedX + containerPadding - centerX) / radius) * 100)
+      const yPercentage = Math.round((((changedY + containerPadding - centerY) * -1) / radius) * 100)
 
       runOnJS(setLeft)(changedX)
       runOnJS(setTop)(changedY)
@@ -88,20 +84,6 @@ const EditScheduleText = ({data, centerX, centerY, radius, titleInputRef, onChan
     }
   })
 
-  const sizeStyle = useAnimatedStyle(() => {
-    return {
-      width: containerWidth.value,
-      height: containerHeight.value
-    }
-  })
-
-  const changeTextInputSize = (e: NativeSyntheticEvent<TextInputContentSizeChangeEventData>) => {
-    const size = e.nativeEvent.contentSize
-    console.log(size)
-    containerWidth.value = size.width
-    containerHeight.value = size.height
-  }
-
   React.useEffect(() => {
     setLeft(containerX.value)
     setTop(containerY.value)
@@ -109,23 +91,20 @@ const EditScheduleText = ({data, centerX, centerY, radius, titleInputRef, onChan
 
   return (
     <GestureDetector gesture={composeGesture}>
-      <Animated.View style={[styles.conatiner, {padding: containerPadding}]}>
-        <Animated.View style={[positionStyle, rotateStyle, {padding: wrapperPadding}]}>
-          <TextInput
-            ref={titleInputRef}
-            value={data.title}
-            placeholder="일정명을 입력해주세요."
-            returnKeyType="done"
-            returnKeyLabel="done"
-            placeholderTextColor="#c3c5cc"
-            style={[styles.textInput, {color: data.text_color}]}
-            maxLength={20}
-            multiline
-            scrollEnabled={false}
-            onChangeText={changeTitle}
-            onContentSizeChange={changeTextInputSize}
-          />
-        </Animated.View>
+      <Animated.View style={[positionStyle, rotateStyle, styles.conatiner, {padding: containerPadding}]}>
+        <TextInput
+          ref={titleInputRef}
+          value={data.title}
+          placeholder="일정명을 입력해주세요."
+          returnKeyType="done"
+          returnKeyLabel="done"
+          placeholderTextColor="#c3c5cc"
+          style={[styles.textInput, {color: data.text_color}]}
+          maxLength={20}
+          multiline
+          scrollEnabled={false}
+          onChangeText={changeTitle}
+        />
       </Animated.View>
     </GestureDetector>
   )
@@ -133,15 +112,9 @@ const EditScheduleText = ({data, centerX, centerY, radius, titleInputRef, onChan
 
 const styles = StyleSheet.create({
   conatiner: {
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    right: 0,
-    bottom: 0
-    // flex: 1
+    position: 'absolute'
   },
   textInput: {
-    position: 'absolute',
     fontFamily: 'Pretendard-Medium',
     fontSize: 16,
     color: '#000',
