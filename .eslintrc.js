af8eaedc6e48d7d65ef79151960cb43b73db3532
Hyunsu <kailking611@gmail.com>
2024-01-19 18:55:23 +0900

    성능 개선용 eslnit 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@react-native', 'prettier'],
+  extends: ['@react-native', 'prettier', 'plugin:react-perf/recommended'],
   rules: {
     'react-native/no-inline-styles': 0
   }
