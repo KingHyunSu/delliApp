39ab4d8e96eeb2e24daad2ecd4f76186b9632165
KingHyunSu <kailking611@gmail.com>
2024-02-06 12:34:46 +0900

    ios 스플래시 화면 추가

--- ios/delli/AppDelegate.mm
+++ ios/delli/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import <RNKakaoLogins.h>
+#import "RNSplashScreen.h"
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -12,7 +13,10 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
   
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [RNSplashScreen show];
+  
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
