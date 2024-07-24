d808bf6acd304b39eda43b5d6291d6661af2f601
Hyunsu <kailking611@gmail.com>
2024-07-24 18:13:47 +0900

    머지 충돌 수정중

--- src/components/WeeklyDatePicker/index.tsx
+++ src/components/WeeklyDatePicker/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { View } from 'react-native'
+import {View} from 'react-native'
 
 import WeekController from './src/WeekController'
 import DayPicker from './src/DayPicker'
 
-import { useRecoilState } from 'recoil'
-import { scheduleDateState } from '@/store/schedule'
+import {useRecoilState} from 'recoil'
+import {scheduleDateState} from '@/store/schedule'
 
-import { getWeeklyDateList } from './util'
+import {getWeeklyDateList} from './util'
 
 const WeeklyDatePicker = () => {
   const [scheduleDate, setScheduleDate] = useRecoilState(scheduleDateState)
