7b52e38cc62ce09155e1a6bd234c43459c7e86a0
Hyunsu <kailking611@gmail.com>
2024-08-10 03:01:31 +0900

    BottomSheetHandler.tsx 그림자 추가

--- src/components/BottomSheetHandler.tsx
+++ src/components/BottomSheetHandler.tsx
@@ -1,8 +1,9 @@
 import React, {useMemo} from 'react'
-import {StyleProp, StyleSheet, ViewStyle} from 'react-native'
+import {StyleSheet, View} from 'react-native'
 import {BottomSheetHandleProps} from '@gorhom/bottom-sheet'
 import Animated, {Extrapolation, interpolate, useAnimatedStyle, useDerivedValue} from 'react-native-reanimated'
 import {toRad} from 'react-native-redash'
+import {Shadow} from 'react-native-shadow-2'
 
 // @ts-ignore
 export const transformOrigin = ({x, y}, ...transformations) => {
@@ -10,26 +11,55 @@ export const transformOrigin = ({x, y}, ...transformations) => {
   return [{translateX: x}, {translateY: y}, ...transformations, {translateX: x * -1}, {translateY: y * -1}]
 }
 
-interface Props extends BottomSheetHandleProps {
-  style?: StyleProp<ViewStyle>
+const shadowSides = {top: true, bottom: false, start: false, end: false}
+const shadowCorners = {
+  topStart: true,
+  topEnd: true,
+  bottomStart: false,
+  bottomEnd: false
 }
 
-const BottomSheetHandler = ({style, animatedIndex}: Props) => {
+interface Props extends BottomSheetHandleProps {
+  maxSnapIndex?: 1 | 2 | 3
+}
+
+const BottomSheetHandler = ({maxSnapIndex = 3, animatedIndex}: Props) => {
+  const inputInterpolate = [-0.6, 0, 1, 2]
+
+  const outputIndicatorTransformOriginY = useMemo(() => {
+    if (maxSnapIndex === 1) {
+      return [0, 1, 1, 1]
+    } else if (maxSnapIndex === 2) {
+      return [0, -1, 1, 1]
+    }
+    return [0, -1, 0, 1]
+  }, [maxSnapIndex])
+
+  const outputLeftIndicator = useMemo(() => {
+    if (maxSnapIndex === 1) {
+      return [0, toRad(30), toRad(30), toRad(30)]
+    } else if (maxSnapIndex === 2) {
+      return [0, toRad(-30), toRad(30), toRad(30)]
+    }
+    return [0, toRad(-30), 0, toRad(30)]
+  }, [maxSnapIndex])
+
+  const outputRightIndicator = useMemo(() => {
+    if (maxSnapIndex === 1) {
+      return [0, toRad(-30), toRad(-30), toRad(-30)]
+    } else if (maxSnapIndex === 2) {
+      return [0, toRad(30), toRad(-30), toRad(-30)]
+    }
+    return [0, toRad(30), 0, toRad(-30)]
+  }, [maxSnapIndex])
+
   //#region animations
   const indicatorTransformOriginY = useDerivedValue(() =>
-    interpolate(animatedIndex.value, [0, 1, 2], [-1, 0, 1], Extrapolation.CLAMP)
+    interpolate(animatedIndex.value, inputInterpolate, outputIndicatorTransformOriginY, Extrapolation.CLAMP)
   )
   //#endregion
 
   //#region styles
-  const containerStyle = useMemo(() => [styles.header, style], [style])
-  const containerAnimatedStyle = useAnimatedStyle(() => {
-    const borderTopRadius = interpolate(animatedIndex.value, [1, 2], [20, 0], Extrapolation.CLAMP)
-    return {
-      borderTopLeftRadius: borderTopRadius,
-      borderTopRightRadius: borderTopRadius
-    }
-  })
   const leftIndicatorStyle = useMemo(
     () => ({
       ...styles.indicator,
@@ -37,11 +67,12 @@ const BottomSheetHandler = ({style, animatedIndex}: Props) => {
     }),
     []
   )
+
   const leftIndicatorAnimatedStyle = useAnimatedStyle(() => {
     const leftIndicatorRotate = interpolate(
       animatedIndex.value,
-      [0, 1, 2],
-      [toRad(-30), 0, toRad(30)],
+      inputInterpolate,
+      outputLeftIndicator,
       Extrapolation.CLAMP
     )
     return {
@@ -56,6 +87,7 @@ const BottomSheetHandler = ({style, animatedIndex}: Props) => {
       )
     }
   })
+
   const rightIndicatorStyle = useMemo(
     () => ({
       ...styles.indicator,
@@ -63,11 +95,12 @@ const BottomSheetHandler = ({style, animatedIndex}: Props) => {
     }),
     []
   )
+
   const rightIndicatorAnimatedStyle = useAnimatedStyle(() => {
     const rightIndicatorRotate = interpolate(
       animatedIndex.value,
-      [0, 1, 2],
-      [toRad(30), 0, toRad(-30)],
+      inputInterpolate,
+      outputRightIndicator,
       Extrapolation.CLAMP
     )
     return {
@@ -84,24 +117,39 @@ const BottomSheetHandler = ({style, animatedIndex}: Props) => {
   })
   //#endregion
 
-  // render
   return (
-    <Animated.View style={[containerStyle, containerAnimatedStyle]} renderToHardwareTextureAndroid={true}>
-      <Animated.View style={[leftIndicatorStyle, leftIndicatorAnimatedStyle]} />
-      <Animated.View style={[rightIndicatorStyle, rightIndicatorAnimatedStyle]} />
-    </Animated.View>
+    <Shadow
+      startColor="#f0eff586"
+      distance={10}
+      sides={shadowSides}
+      corners={shadowCorners}
+      style={styles.shadowContainer}>
+      <View style={styles.header} renderToHardwareTextureAndroid={true}>
+        <Animated.View style={[leftIndicatorStyle, leftIndicatorAnimatedStyle]} />
+        <Animated.View style={[rightIndicatorStyle, rightIndicatorAnimatedStyle]} />
+      </View>
+    </Shadow>
   )
 }
 
 const styles = StyleSheet.create({
+  shadowContainer: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderTopWidth: 1,
+    borderColor: '#e7e7eb89',
+
+    width: '100%',
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30
+  },
   header: {
     alignContent: 'center',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'white',
-    paddingVertical: 14,
-    borderBottomWidth: 1,
-    borderBottomColor: '#fff'
+    paddingTop: 20,
+    paddingBottom: 10
   },
   indicator: {
     position: 'absolute',
