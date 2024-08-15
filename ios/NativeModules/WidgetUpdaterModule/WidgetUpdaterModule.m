821355c59f16016b4c401f1916d6a9bf989b1d66
Hyunsu <kailking611@gmail.com>
2024-08-15 23:12:20 +0900

    ios 버그 수정

---
+++ ios/NativeModules/WidgetUpdaterModule/WidgetUpdaterModule.m
@@ -0,0 +1,14 @@
+//
+//  WidgetUpdaterModule.m
+//  delli
+//
+//  Created by 김현수 on 7/6/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(WidgetUpdaterModule, NSObject)
+RCT_EXTERN_METHOD(updateWidget: (NSString *)data)
+RCT_EXTERN_METHOD(shouldWidgetReload: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject)
+@end
