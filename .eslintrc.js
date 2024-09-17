3227c23ffa3752bf6a5bdb0397eb42c72d29f839
Hyunsu <kailking611@gmail.com>
2024-09-17 20:20:57 +0900

    eslint 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: ['@react-native', 'prettier', 'plugin:react-perf/recommended'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-native/no-inline-styles': 0
   }
 }
